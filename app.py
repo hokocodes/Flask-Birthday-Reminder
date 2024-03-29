8da6a653fbc23fc8a0be554b163d3065e1e50b3e
Hoko <94093120+hokocodes@users.noreply.github.com>
2024-03-29 14:04:50 -0500

    Updated user model to add birthday

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -30,7 +30,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-
+    birthday = db.Column(db.DateTime, nullable=False)
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[
